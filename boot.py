db3d1b575281f19513f5d0a28e5785ed549726e0
Andy <ndmaque@gmail.com>
2023-05-17 13:52:25 +0100

    added ugit library, moded boot.py

--- boot.py
+++ boot.py
@@ -11,6 +11,10 @@ esp.osdebug(None)
 import gc
 gc.collect()
 
+import ugit
+# https://raw.githubusercontent.com/ndmaque/haSensorStation/main/README.md
+
+
 ssid = 'TP-Link_9162'
 password = '65919675'
 mqtt_server = '192.168.1.20'
@@ -23,5 +27,9 @@ station.connect(ssid, password)
 
 while station.isconnected() == False:
   pass
+
+#ugit.pull('README.md','https://raw.githubusercontent.com/ndmaque/haSensorStation/main/README.md')
+#def pull_all(tree=call_trees_url,raw = raw,ignore = ignore,isconnected=False)
+ugit.pull_all(isconnected=True)
 
 print(station.ifconfig())
